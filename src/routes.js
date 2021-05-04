c9a77958177b8648dbe6ebbc4365433c3f9b4df9
Desson Medeiros <dessonwms@gmail.com>
2021-05-03 20:47:05 -0400

    CRUD  Products

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const routes = express.Router()
-const instructors = require('./app/controllers/instructors')
-const members = require('./app/controllers/members')
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function(req, res){
-    return res.send('OK')
+    return res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductController.create)
+routes.get('/products/:id/edit', ProductController.edit)
+routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+
+// Alias
+routes.get('/ads/create', function(req, res){
+    return res.redirect("/products/create")
 })
 
 module.exports = routes
